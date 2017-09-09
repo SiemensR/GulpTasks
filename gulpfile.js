07873007d1d77dce258f73b484493ed0206e1ced
Антон <anton@MacBook-Air-Anton.local>
2017-09-09 23:55:11 +0200

    working stand with css and html

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
    });
 });
   gulp.task('sass', function(){ // Создаем таск "sass"
-      return gulp.src('sass/main.sass') // Берем источник
+      return gulp.src('sass/main.scss') // Берем источник
           .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
           .pipe(gulp.dest('css/')) // Выгружаем результата в папку app/css
           .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
@@ -28,8 +28,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 // });
 -->
  gulp.task('watch',['browser-sync','sass'], function() {
-     gulp.watch('sass/*.sass', ['sass']); // Наблюдение за sass файлами
-     gulp.watch('*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+     gulp.watch('sass/*.scss', ['sass']); // Наблюдение за sass файлами
+     gulp.watch('**/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
      gulp.watch('css/**/*.css', browserSync.reload);
      gulp.watch('js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
  });
